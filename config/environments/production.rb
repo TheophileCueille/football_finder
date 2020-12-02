baa4321478231549116f9e9f88a85bd9516c9b42
Théophile <theophile.cueille@gmail.com>
2020-12-02 12:23:42 +0100

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "football_finder_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+ 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
