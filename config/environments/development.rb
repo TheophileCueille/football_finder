6576b404d693671b51ceb836ec87d42b6faff588
Théophile <theophile.cueille@gmail.com>
2020-12-02 16:42:01 +0100

    update remote devise master

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }#devise
   config.action_mailer.default_url_options = { :host => 'https://foot-develop.herokuapp.com/'}
   #letter opener
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
