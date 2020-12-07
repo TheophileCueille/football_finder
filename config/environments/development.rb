53755a5139a24efe6ef4ce069ab0392a89739410
Yanis763 <yanispsn76@hotmail.com>
2020-12-07 16:27:18 +0100

    mailer 2.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }#devise
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }#devise
   config.action_mailer.default_url_options = { :host => 'https://foot-develop.herokuapp.com'}
   #letter opener
   config.action_mailer.delivery_method = :smtp
--- db/schema.rb
+++ db/schema.rb
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_111437) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "firstname", null: false
-    t.string "lastname", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
