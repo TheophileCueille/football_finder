e39317d968fad5d53d94a6b44af2bad4139dac4a
Yanis763 <yanispsn76@hotmail.com>
2020-12-08 14:10:56 +0100

    debug

--- app/controllers/compets_controller.rb
+++ app/controllers/compets_controller.rb
@@ -26,7 +26,7 @@ class CompetsController < ApplicationController
         @teams_show = self.class.get("/teams/#{params[:id]}", @options)
 
         @new_address = Compet.create address: @teams_show["venue"] #faire aussi pour team id 
-
     end
+end
 
  
